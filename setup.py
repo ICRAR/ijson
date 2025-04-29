609fa510cd7516670a85b931e4557159dd5ade42
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-04-30 01:52:54 +0200

    Patch yajl sources for isolated build environments

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ import tempfile
 from setuptools import setup, Extension
 from setuptools._distutils import ccompiler
 from setuptools._distutils import sysconfig
+from setuptools.command.build_ext import build_ext
+from setuptools.command.sdist import sdist
 
 setupArgs = {}
 
@@ -48,19 +50,48 @@ def yajl_present():
         os.remove(yajl_version_test_file.name)
 
 
-def patch_yajl_sources():
-    """Make yajl sources ready for direct compilation against them"""
+ORIGINAL_SOURCES = os.path.join("cextern", "yajl")
+PATCHED_SOURCES = "yajl"
+
+
+def patch_yajl_sources() -> None:
+    """Make yajl sources ready for direct compilation against them."""
     # cp cextern/yajl -R $yajl_sources_copy
     # mkdir $yajl_sources_copy/yajl
     # cp $yajl_sources_copy/src/api/*.h $yajl_sources_copy/yajl
-    patched_sources = os.path.join(tempfile.mkdtemp(), 'yajl')
-    shutil.copytree(os.path.join('cextern', 'yajl'), patched_sources)
-    headers_original = os.path.join(patched_sources, 'src', 'api')
-    headers_copy = os.path.join(patched_sources, 'yajl')
+    if os.path.isdir(PATCHED_SOURCES):
+        print("Sources are already in the correct place. Skip copy")
+        return
+    shutil.copytree(
+        ORIGINAL_SOURCES, PATCHED_SOURCES,
+        ignore=shutil.ignore_patterns("example", "test"),
+    )
+    headers_original = os.path.join(PATCHED_SOURCES, "src", "api")
+    headers_copy = os.path.join(PATCHED_SOURCES, "yajl")
     shutil.copytree(headers_original, headers_copy)
-    return patched_sources
 
 
+def cleanup_yajl_sources() -> None:
+    """Cleanup yajl sources."""
+    if os.path.isdir(PATCHED_SOURCES):
+        shutil.rmtree(PATCHED_SOURCES)
+
+
+class CustomSDist(sdist):
+    def run(self) -> None:
+        self.execute(patch_yajl_sources, ())
+        sdist.run(self)
+        self.execute(cleanup_yajl_sources, ())
+
+
+class CustomBuildExt(build_ext):
+    def run(self) -> None:
+        self.execute(patch_yajl_sources, ())
+        build_ext.run(self)
+        self.execute(cleanup_yajl_sources, ())
+
+
+cmdclass = {}
 extra_sources = []
 extra_include_dirs = []
 libs = ['yajl']
@@ -68,10 +99,19 @@ embed_yajl = os.environ.get('IJSON_EMBED_YAJL', None) == '1'
 if not embed_yajl:
     have_yajl = yajl_present()
 else:
-    yajl_sources = patch_yajl_sources()
+    cmdclass = {"sdist": CustomSDist, "build_ext": CustomBuildExt}
+    if not os.path.isdir(PATCHED_SOURCES):
+        # sdist: First pass in the source tree.
+        # Files haven't been copied yet.
+        yajl_sources = ORIGINAL_SOURCES
+    else:
+        # wheel: Second pass in the isolated build environment.
+        # Only copied files are available.
+        yajl_sources = PATCHED_SOURCES
     extra_sources = sorted(glob.glob(os.path.join(yajl_sources, 'src', '*.c')))
     extra_sources.remove(os.path.join(yajl_sources, 'src', 'yajl_version.c'))
-    extra_include_dirs = [yajl_sources, os.path.join(yajl_sources, 'src')]
+    extra_sources = [p.replace(yajl_sources, PATCHED_SOURCES) for p in extra_sources]
+    extra_include_dirs = [PATCHED_SOURCES, os.path.join(PATCHED_SOURCES, 'src')]
     libs = []
 build_yajl_default = '1' if embed_yajl or have_yajl else '0'
 build_yajl = os.environ.get('IJSON_BUILD_YAJL2C', build_yajl_default) == '1'
@@ -83,5 +123,6 @@ if build_yajl:
                          libraries=libs,
                          depends=glob.glob('src/ijson/backends/ext/_yajl2/*.h'))
     setupArgs['ext_modules'] = [yajl_ext]
+setupArgs["cmdclass"] = cmdclass
 
 setup(**setupArgs)
